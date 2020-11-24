1991a75a58ab26046be807e7f1e0209f1c871e1d
Marco Segato <marco.segato@gmail.com>
2020-11-24 17:04:47 +0100

    Lista delle province

---
+++ lista_province.php
@@ -0,0 +1,8 @@
+<option value="index.php?tipo=PROV&ente=AL">Alessandria</option>
+<option value="index.php?tipo=PROV&ente=AT">Asti</option>
+<option value="index.php?tipo=PROV&ente=BI">Biella</option>
+<option value="index.php?tipo=PROV&ente=CN">Cuneo</option>
+<option value="index.php?tipo=PROV&ente=NO">Novara</option>
+<option value="index.php?tipo=PROV&ente=TO">Torino</option>
+<option value="index.php?tipo=PROV&ente=VCO">Verbano-Cusio-Ossola</option>
+<option value="index.php?tipo=PROV&ente=VC">Vercelli</option>
